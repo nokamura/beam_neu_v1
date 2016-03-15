09ee27319a6bac7c257396c09850120c481decd7
useakat@kekcc <useakat@gmail.com>
2016-03-15 17:25:05 +0900

    set_xsec.sh modified: implement to make xsec_dir.txt in xsecCC(NC) dir

--- set_xsec.sh
+++ set_xsec.sh
@@ -8,6 +8,7 @@ NC_flag=0
 if [ $dirCC != "0" ];then
     rm -rf xsecCC
     cp -rf $dirCC xsecCC
+    echo $dirCC > xsecCC/xsecCC_dir.txt
     CC_flag=1
 else
     echo "skip to set xsecCC"
@@ -15,6 +16,7 @@ fi
 if [ $dirNC != "0" ];then
     rm -rf xsecNC
     cp -rf $dirNC xsecNC
+    echo $dirNC > xsecNC/xsecNC_dir.txt
     NC_flag=1
 else
     echo "skip to set xsecNC"
